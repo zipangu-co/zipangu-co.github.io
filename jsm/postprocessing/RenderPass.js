b35545c15a04878a295110776d5b5648c2bb5536
edw <40938410+edwardigarashi@users.noreply.github.com>
2021-03-02 19:17:39 -0800

    Update RenderPass.js

--- jsm/postprocessing/RenderPass.js
+++ jsm/postprocessing/RenderPass.js
@@ -1,6 +1,6 @@
 import {
 	Color
-} from '../../../build/three.module.js';
+} from 'https://threejs.org/build/three.module.js';
 import { Pass } from '../postprocessing/Pass.js';
 
 var RenderPass = function ( scene, camera, overrideMaterial, clearColor, clearAlpha ) {
